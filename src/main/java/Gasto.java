4b465abf1014f7ceb3b602206ec830e163c02702
Alejandro Valenzuela 1 <avalenzuela@uno.com>
2020-12-20 21:09:03 -0300

    Implementando Gastos

--- src/main/java/Gasto.java
+++ src/main/java/Gasto.java
@@ -1,2 +1,15 @@
 public class Gasto {
+    private String nombre;
+    private double valor;
+    private boolean pagado;
+
+    public Gasto(String nombre, double valor, boolean pagado) {
+        this.nombre = nombre;
+        this.valor = valor;
+        this.pagado = pagado;
+    }
+
+    public double getValor() {
+        return valor;
+    }
 }
